f5fbc057b80e7a880255bf2d6580ab2f767ef914
Ping <artistic709@gmail.com>
2019-10-03 18:43:08 +0800

    Create CryptoStructuredFund.sol

---
+++ CryptoStructuredFund.sol
@@ -0,0 +1,284 @@
+pragma solidity ^0.5.10;
+
+library SafeMath {
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) 
+            return 0;
+        uint256 c = a * b;
+        require(c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0);
+        uint256 c = a / b;
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        uint256 c = a - b;
+        return c;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a);
+        return c;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b != 0);
+        return a % b;
+    }
+}
+
+
+contract ERC20 {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) internal _balances;
+    mapping (address => mapping (address => uint256)) internal _allowed;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    uint256 internal _totalSupply;
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param owner The address to query the balance of.
+    * @return A uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address owner) public view returns (uint256) {
+        return _balances[owner];
+    }
+
+    /**
+    * @dev Function to check the amount of tokens that an owner allowed to a spender.
+    * @param owner address The address which owns the funds.
+    * @param spender address The address which will spend the funds.
+    * @return A uint256 specifying the amount of tokens still available for the spender.
+    */
+    function allowance(address owner, address spender) public view returns (uint256) {
+        return _allowed[owner][spender];
+    }
+
+    /**
+    * @dev Transfer token to a specified address
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
+    function transfer(address to, uint256 value) public returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    /**
+    * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+    * Beware that changing an allowance with this method brings the risk that someone may use both the old
+    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+    * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+    * @param spender The address which will spend the funds.
+    * @param value The amount of tokens to be spent.
+    */
+    function approve(address spender, uint256 value) public returns (bool) {
+        _allowed[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    /**
+    * @dev Transfer tokens from one address to another.
+    * Note that while this function emits an Approval event, this is not required as per the specification,
+    * and other compliant implementations may not emit the event.
+    * @param from address The address which you want to send tokens from
+    * @param to address The address which you want to transfer to
+    * @param value uint256 the amount of tokens to be transferred
+    */
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
+        _transfer(from, to, value);
+        _allowed[msg.sender][to] = _allowed[msg.sender][to].sub(value);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal {
+        require(to != address(0));
+        _balances[from] = _balances[from].sub(value);
+        _balances[to] = _balances[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+}
+
+contract ERC20Mintable is ERC20 {
+
+    function _mint(address to, uint256 amount) internal {
+        _balances[to] = _balances[to].add(amount);
+        _totalSupply = _totalSupply.add(amount);
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        _balances[from] = _balances[from].sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(from, address(0), amount);
+    }
+
+}
+
+
+contract Exchange {
+    function swapEtherToToken(address token, uint minConversionRate) public payable returns(uint);
+    function swapTokenToEther(address token, uint srcAmount, uint minConversionRate) public returns(uint);
+}
+
+contract CryptoStructuredFund is ERC20Mintable {
+
+    using SafeMath for uint256;
+
+    string public constant name = "CryptoStructuredFund";
+    string public constant symbol = "CSF";
+    uint8 public constant decimals = 18;
+
+    uint256 public startBuy;
+    uint256 public stopBuy;
+    uint256 public startSell;
+    uint256 public stopSell;
+    uint256 public rate;
+    bool public fulfilled;
+    address payable wallet;
+
+    uint256 constant fee = 8e15;
+
+    mapping(address => uint256) public investmentOf;
+    uint256 public totalInvestment;
+
+    Exchange kyberEx = Exchange(0x818E6FECD516Ecc3849DAf6845e3EC868087B755);
+    ERC20 DAI = ERC20(0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359);
+
+    function set(uint256 _startBuy, uint256 _stopBuy, uint256 _startSell, uint256 _stopSell, uint256 _rate, address payable _wallet) public {
+        require(stopBuy == 0 && stopSell == 0 && wallet == address(0));
+        startBuy = _startBuy;
+        stopBuy = _stopBuy;
+        startSell = _startSell;
+        stopSell = _stopSell;
+        rate = _rate;
+        wallet = _wallet;
+        DAI.approve(address(kyberEx), uint256(-1));
+    }
+
+    // deposit DAI to earn interest
+    function deposit(uint256 amount) public {
+        require(now < startBuy);
+        require(DAI.transferFrom(msg.sender, address(this), amount));
+        require(DAI.transfer(wallet, amount.mul(fee).div(1e18)));
+        _mint(msg.sender, amount);
+    }
+
+    // invest ether
+    function invest() public payable {
+        require(now < startBuy);
+        require(wallet.send(msg.value.mul(fee).div(1e18)));
+        investmentOf[msg.sender] = investmentOf[msg.sender].add(msg.value);
+        totalInvestment = totalInvestment.add(msg.value);
+    }
+
+    // withdraw DAI
+    function withdraw(uint256 amount) public {
+        require(now > startSell);
+        
+        uint256 withdrawal = amount.mul(DAI.balanceOf(address(this))).div(_totalSupply);
+        _burn(msg.sender, amount);
+
+        DAI.transfer(msg.sender, withdrawal);
+    }
+
+    // redeem ether
+    function redeem(uint256 amount) public {
+        require(fulfilled);
+
+        uint256 withdrawal = amount.mul(address(this).balance).div(totalInvestment);
+        investmentOf[msg.sender] = investmentOf[msg.sender].sub(amount);
+        totalInvestment = totalInvestment.sub(amount);
+
+        msg.sender.transfer(withdrawal);
+    }
+
+    //buy ether via KyberSwap
+    function push() public {
+        require(now > startBuy && now < startSell);
+        uint256 portion = interpolation(startBuy, stopBuy, now);
+        startBuy = now;
+
+        uint256 amount = DAI.balanceOf(address(this)).mul(portion).div(1e18);
+        uint256 IncomingEther = kyberEx.swapTokenToEther(address(DAI), amount, 1);
+
+        msg.sender.transfer(IncomingEther.div(1000)); // 0.1% rebate to pusher
+    }
+
+    //sell ether to DAI 
+    function pull() public {
+        require(now > startSell && !fulfilled);
+        uint256 portion = interpolation(startSell, stopSell, now);
+        startSell = now;
+
+        uint256 amount = address(this).balance.mul(portion).div(1e18);
+        uint256 IncomingDAI = kyberEx.swapEtherToToken.value(amount)(address(DAI), 1);
+
+        DAI.transfer(msg.sender, IncomingDAI.div(1000)); // 0.1% rebate to pusher
+
+
+        if(DAI.balanceOf(address(this)) >= _totalSupply.mul(rate).div(1e18))
+            fulfilled = true;
+
+    }
+
+    function interpolation(uint256 a, uint256 b, uint256 c) internal pure returns(uint256) {
+        if(c > b) return 1e18;
+        // (c - a) / (b - a) * 1e18
+        return c.sub(a).mul(1e18).div(b.sub(a));
+    }
+
+    function () external payable {} // accept ether
+
+}
+
+contract factory {
+    using SafeMath for uint256;
+    
+    address public template = address(0x692a70D2e424a56D2C6C27aA97D1a86395877b3A);
+
+    address[] public CSFs;
+
+    function newCSF(uint256 startTime, uint256 period, uint256 duration, uint256 rate, address payable wallet) public {
+        address payable CSF = address(uint160(createClone(template)));
+        CryptoStructuredFund(CSF).set(startTime, startTime.add(period), startTime.add(duration), startTime.add(period).add(duration), rate, wallet);
+        CSFs.push(CSF);
+    }
+
+    function getCSFs() public view returns(address[] memory) {
+        return CSFs;
+    }
+
+    function createClone(address target) internal returns (address result) {
+        bytes20 targetBytes = bytes20(target);
+        assembly {
+            let clone := mload(0x40)
+            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(clone, 0x14), targetBytes)
+            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            result := create(0, clone, 0x37)
+        }
+    }
+
+}
